f08394f7e321ae7b42a3c42f2136d89371137f15
urvashigupta7 <urvashi072000@gmail.com>
2020-05-01 09:11:54 +0530

    update

--- public/script/app.js
+++ public/script/app.js
@@ -23,3 +23,18 @@ else{
     msg.innerText="";
 }
 }
+var i=0;
+var text="Want To Write Something, Give It A Try Here !";
+var speed = 100; 
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("type").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+if(i!==text.length){
+setTimeout(typeWriter,speed);
+}
+
